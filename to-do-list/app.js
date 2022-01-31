35715f8c6f34e3aea2b570eb2913c46a6338cd52
Vipan <vipanp09@gmail.com>
2022-01-31 21:35:53 +0530

    To-do App setup

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,11 +1,54 @@
-// Selectors
+//Selector
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list')
+const todoList = document.querySelector('.todo-list');
 
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
-// Event Listeners
+//Functions
+function addTodo(event){
+    // Prevent form from submitting
+  event.preventDefault();
+  // Todo Div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add("todo");
+  // Create Li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
 
+  // Check mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerText = 'Complete';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
 
+  // Check Trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerText = 'Delete';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
 
-// Functions
+  //Append to List
+  todoList.appendChild(todoDiv)
+
+  //Clear Todo Input Value
+  todoInput.value = "";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    // Delet To DO
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    //Check Mark
+    if(item.classList[0] ===  "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
